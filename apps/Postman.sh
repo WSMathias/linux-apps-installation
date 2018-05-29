b9d6f65adae5ff826d460334d82c48cdd289d946
Wilson Samuel Mathias <wsmsamuel@gmail.com>
2018-05-29 09:41:02 +0530

    Create Postman.sh

---
+++ apps/Postman.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
+tar -xzf postman.tar.gz -C /opt
+ln -s /opt/Postman/Postman /usr/bin/postman
+rm postman.tar.gz
+cat > ~/.local/share/applications/postman.desktop <<EOL
+[Desktop Entry]
+Encoding=UTF-8
+Name=Postman
+Exec=postman
+Icon=/opt/Postman/resources/app/assets/icon.png
+Terminal=false
+Type=Application
+Categories=Development;
+EOL
